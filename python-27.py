a1652a0abec16b2699f87963d1475089c34301c7
William Bert <william@spanishdict.com>
2017-09-10 13:24:45 -0700

    Add initial skeletons.

---
+++ python-27.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+"""
+A skeleton python script which reads from an input file,
+writes to an output file and parses command line arguments.
+"""
+from __future__ import print_function
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument(
+        "infile", nargs="?", default="-",
+        metavar="INPUT_FILE", type=argparse.FileType("r"),
+        help="path to the input file (read from stdin if omitted)")
+
+    parser.add_argument(
+        "outfile", nargs="?", default="-",
+        metavar="OUTPUT_FILE", type=argparse.FileType("w"),
+        help="path to the output file (write to stdout if omitted)")
+
+    args = parser.parse_args()
+
+    for line in args.infile:
+        print(line.strip(), file=args.outfile)
+
+
+if __name__ == "__main__":
+    main()
---
+++ stream.js
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+/*eslint no-console: 0*/
+
+/*
+# Usage
+
+$ stream.js --infile <infile.json> --outfile <outfile.json>
+OR
+$ < infile.json stream.js > outfile.json
+*/
+
+const fs = require('fs');
+const ndjson = require('ndjson');
+const through = require('through2');
+const request = require('request');
+const argv = require('minimist')(process.argv.slice(2));
+
+const infile = argv.infile;
+const outfile = argv.outfile;
+const instream = infile ? fs.createReadStream(infile) : process.stdin;
+const outstream = outfile ? fs.createWriteStream(outfile) : process.stdout;
+
+function doSomething(obj, enc, cb) {
+  // Do something async with obj, for example:
+  let url = 'https://www.googleapis.com/customsearch/v1' +
+      `?key=${API_KEY}&cx=017576662512468239146:omuauf_lfve` +
+      `&q=${encodeURIComponent(obj)}`
+  request(
+    url,
+    {
+      json: true,
+    },
+    function(err, res, body) {
+      if (res.statusCode === 200) {
+        obj.result = body.data.source;
+      }
+      cb(err, obj);
+    }
+  );
+}
+
+function finish(err) {
+  if (err) {
+    console.error(err);
+    return process.exit(1);
+  }
+}
+
+if (require.main === module) {
+  instream
+    .pipe(ndjson.parse())
+    .pipe(through.obj(doSomething))
+    .on('error', finish)
+    .pipe(ndjson.serialize())
+    .on('end', finish)
+    .resume()
+    .pipe(outstream);
+}
+
+module.exports = exports;
